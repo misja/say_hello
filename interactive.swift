823bd89d15ac3cf640240ab769914b002eaa234d
Misja Hoebe <misja.hoebe@pm.me>
2022-02-12 20:50:57 +0100

    Formatted swift version

---
+++ interactive.swift
@@ -0,0 +1,23 @@
+///  Ask and remember a user's name and say hello
+///
+/// Run as:
+///    swift interactive.swift
+
+var running = true
+var names: [String] = []
+
+while running {
+  print("Your name: ")
+
+  if let value = readLine() {
+    if value == "exit" {
+      print("Bye!")
+      running = false
+    } else if names.contains(value) {
+      print("Hallo \(value), we have met before :)")
+    } else {
+      names.append(value)
+      print("Hi \(value)!")
+    }
+  }
+}
